a35fcabd92ab153c2498c3898822612902cbc3b1
Murylo Marques <murylobrayan@gmail.com>
2025-12-05 11:22:33 -0300

    Subindo AT de FCA e Remover laudos14

--- app/Http/Controllers/VistoriaSegurancaController.php
+++ app/Http/Controllers/VistoriaSegurancaController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator; // Adicione este import
 
 class VistoriaSegurancaController extends Controller
